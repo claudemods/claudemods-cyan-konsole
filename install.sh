ed449aa1320a1cdf67578a53a7c13b0b5de11b5d
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-04 20:11:52 +0000

    Update and rename install.sh to installer.sh

--- install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/$USER && git clone https://github.com/claudemods/claudemods-cyan-konsole
-cd /home/$USER/claudemods-cyan-konsole && chmod +x * && ./install.sh
---
+++ installer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/$USER && git clone https://github.com/claudemods/claudemods-cyan-konsole
+cp -r /home/$USER/claudemods-cyan-konsole/claudemods-cyan.colorscheme /home/$USER/.local/share/konsole
+cp -r /home/$USER/claudemods-cyan-konsole/claudemods-cyan.profile /home/$USER/.local/share/konsole
+sed -i '/^\[Desktop Entry\]/,/^\[/ s/^DefaultProfile=.*/DefaultProfile=claudemods-cyan.profile/' ~/.config/konsolerc
+
