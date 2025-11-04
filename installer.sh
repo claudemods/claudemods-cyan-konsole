83000edb55f2e14de4e213dca22207b5df48a424
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-04 20:15:05 +0000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,4 +3,7 @@ cd /home/$USER && git clone https://github.com/claudemods/claudemods-cyan-konsol
 cp -r /home/$USER/claudemods-cyan-konsole/claudemods-cyan.colorscheme /home/$USER/.local/share/konsole
 cp -r /home/$USER/claudemods-cyan-konsole/claudemods-cyan.profile /home/$USER/.local/share/konsole
 sed -i '/^\[Desktop Entry\]/,/^\[/ s/^DefaultProfile=.*/DefaultProfile=claudemods-cyan.profile/' ~/.config/konsolerc
+rm -rf /home/$USER/claudemods-cyan-konsole
+echo claudemods cyan konsole scheme installed
+konsole
 
